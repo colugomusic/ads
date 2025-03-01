dd2e5fe445f63715139429bffddd4f73a8da79d2
Colugo <colugomusic@gmail.com>
2025-03-01 12:16:33 +0000

    Update main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -13,14 +13,17 @@ TEST_CASE("mono dynamic write/read") {
 	auto st = ads::make_mono({512});
 	REQUIRE (st.get_channel_count() == ads::channel_count{1});
 	REQUIRE (st.get_frame_count() == ads::frame_count{512});
-	st.write(ads::frame_count{512}, [](float* buffer, ads::frame_idx start, ads::frame_count frame_count) {
+	ads::frame_count count;
+	count = st.write(ads::frame_count{512}, [](float* buffer, ads::frame_idx start, ads::frame_count frame_count) {
 		std::iota(buffer, buffer + frame_count.value, 0.0f);
 		return ads::frame_count{frame_count.value};
 	});
-	st.read(ads::frame_count{512}, [](const float* buffer, ads::frame_idx start, ads::frame_count frame_count) {
+	REQUIRE (count == ads::frame_count{512});
+	count = st.read(ads::frame_count{512}, [](const float* buffer, ads::frame_idx start, ads::frame_count frame_count) {
 		for (auto i = 0; i < frame_count.value; ++i) {
 			CHECK (buffer[i] == doctest::Approx(i));
 		}
 		return ads::frame_count{frame_count.value};
 	});
+	REQUIRE (count == ads::frame_count{512});
 }
