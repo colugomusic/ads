be2563799e5b182c9844de2d79e0a25010cf410a
Colugo <colugomusic@gmail.com>
2025-05-31 14:58:22 +0100

    Update ads-vocab.hpp

--- include/ads/ads-vocab.hpp
+++ include/ads/ads-vocab.hpp
@@ -13,7 +13,7 @@ struct channel_idx   { uint64_t value = 0; };
 struct frame_count   { uint64_t value = 0; };
 struct frame_idx     { uint64_t value = 0; };
 
-[[nodiscard]] constexpr inline auto operator*(frame_count lhs, std::integral auto rhs) -> frame_idx   { return {lhs.value * rhs}; }
+[[nodiscard]] constexpr inline auto operator*(frame_count lhs, std::integral auto rhs) -> frame_count { return {lhs.value * rhs}; }
 [[nodiscard]] constexpr inline auto operator*(frame_idx lhs, std::integral auto rhs) -> frame_idx     { return {lhs.value * rhs}; }
 [[nodiscard]] constexpr inline auto operator*(std::integral auto lhs, frame_idx rhs) -> frame_idx     { return {lhs * rhs.value}; }
 [[nodiscard]] constexpr inline auto operator+(double lhs, frame_count rhs) -> double                  { return lhs + rhs.value; }
@@ -48,38 +48,39 @@ struct frame_idx     { uint64_t value = 0; };
 [[nodiscard]] constexpr inline auto operator<=>(frame_idx lhs, concepts::arithmetic auto rhs)     { return lhs.value <=> rhs; }
 [[nodiscard]] constexpr inline auto operator<=>(frame_idx lhs, frame_count rhs)                   { return lhs.value <=> rhs.value; }
 [[nodiscard]] constexpr inline auto operator<=>(frame_idx lhs, frame_idx rhs)                     { return lhs.value <=> rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(channel_count lhs, channel_count rhs)       { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(channel_count lhs, std::integral auto rhs)  { return lhs.value == rhs; }
-[[nodiscard]] constexpr inline auto operator==(channel_count lhs, channel_idx rhs)         { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(channel_idx lhs, channel_count rhs)         { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(channel_idx lhs, channel_idx rhs)           { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(frame_count lhs, frame_count rhs)           { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(frame_count lhs, frame_idx rhs)             { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(frame_count lhs, std::integral auto rhs)    { return lhs.value == rhs; }
-[[nodiscard]] constexpr inline auto operator==(frame_idx lhs, frame_count rhs)             { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(frame_idx lhs, frame_idx rhs)               { return lhs.value == rhs.value; }
-[[nodiscard]] constexpr inline auto operator==(frame_idx lhs, std::integral auto rhs)      { return lhs.value == rhs; }
-[[nodiscard]] constexpr inline auto operator!=(channel_count lhs, std::integral auto rhs)  { return lhs.value != rhs; }
-[[nodiscard]] constexpr inline auto operator!=(channel_count lhs, channel_count rhs)       { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(channel_count lhs, channel_idx rhs)         { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(channel_idx lhs, std::integral auto rhs)    { return lhs.value != rhs; }
-[[nodiscard]] constexpr inline auto operator!=(channel_idx lhs, channel_count rhs)         { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(channel_idx lhs, channel_idx rhs)           { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(frame_count lhs, frame_count rhs)           { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(frame_count lhs, frame_idx rhs)             { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(frame_count lhs, std::integral auto rhs)    { return lhs.value != rhs; }
-[[nodiscard]] constexpr inline auto operator!=(frame_idx lhs, frame_count rhs)             { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(frame_idx lhs, frame_idx rhs)               { return lhs.value != rhs.value; }
-[[nodiscard]] constexpr inline auto operator!=(frame_idx lhs, std::integral auto rhs)      { return lhs.value != rhs; }
-constexpr inline auto operator+=(float& lhs, frame_count rhs) -> float&                    { lhs += rhs.value; return lhs; }
-constexpr inline auto operator+=(frame_count& lhs, frame_count rhs) -> frame_count&        { lhs.value += rhs.value; return lhs; }
-constexpr inline auto operator+=(frame_count& lhs, std::integral auto rhs) -> frame_count& { lhs.value += rhs; return lhs; }
-constexpr inline auto operator-=(frame_count& lhs, frame_count rhs) -> frame_count&        { lhs.value -= rhs.value; return lhs; }
-constexpr inline auto operator-=(frame_count& lhs, std::integral auto rhs) -> frame_count& { lhs.value -= rhs; return lhs; }
-constexpr inline auto operator+=(frame_idx& lhs, frame_count rhs) -> frame_idx&            { lhs.value += rhs.value; return lhs; }
-constexpr inline auto operator+=(frame_idx& lhs, std::integral auto rhs) -> frame_idx&     { lhs.value += rhs; return lhs; }
-constexpr inline auto operator/=(frame_count& lhs, std::integral auto rhs) -> frame_count& { lhs.value /= rhs; return lhs; }
-constexpr inline auto operator/=(frame_idx& lhs, std::integral auto rhs) -> frame_idx&     { lhs.value /= rhs; return lhs; }
+[[nodiscard]] constexpr inline auto operator==(channel_count lhs, channel_count rhs)         { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(channel_count lhs, std::integral auto rhs)    { return lhs.value == rhs; }
+[[nodiscard]] constexpr inline auto operator==(channel_count lhs, channel_idx rhs)           { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(channel_idx lhs, channel_count rhs)           { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(channel_idx lhs, channel_idx rhs)             { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(frame_count lhs, frame_count rhs)             { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(frame_count lhs, frame_idx rhs)               { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(frame_count lhs, std::integral auto rhs)      { return lhs.value == rhs; }
+[[nodiscard]] constexpr inline auto operator==(frame_idx lhs, frame_count rhs)               { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(frame_idx lhs, frame_idx rhs)                 { return lhs.value == rhs.value; }
+[[nodiscard]] constexpr inline auto operator==(frame_idx lhs, std::integral auto rhs)        { return lhs.value == rhs; }
+[[nodiscard]] constexpr inline auto operator!=(channel_count lhs, std::integral auto rhs)    { return lhs.value != rhs; }
+[[nodiscard]] constexpr inline auto operator!=(channel_count lhs, channel_count rhs)         { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(channel_count lhs, channel_idx rhs)           { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(channel_idx lhs, std::integral auto rhs)      { return lhs.value != rhs; }
+[[nodiscard]] constexpr inline auto operator!=(channel_idx lhs, channel_count rhs)           { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(channel_idx lhs, channel_idx rhs)             { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(frame_count lhs, frame_count rhs)             { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(frame_count lhs, frame_idx rhs)               { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(frame_count lhs, std::integral auto rhs)      { return lhs.value != rhs; }
+[[nodiscard]] constexpr inline auto operator!=(frame_idx lhs, frame_count rhs)               { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(frame_idx lhs, frame_idx rhs)                 { return lhs.value != rhs.value; }
+[[nodiscard]] constexpr inline auto operator!=(frame_idx lhs, std::integral auto rhs)        { return lhs.value != rhs; }
+constexpr inline auto operator+=(float& lhs, frame_count rhs) -> float&                      { lhs += rhs.value; return lhs; }
+constexpr inline auto operator+=(frame_count& lhs, frame_count rhs) -> frame_count&          { lhs.value += rhs.value; return lhs; }
+constexpr inline auto operator+=(frame_count& lhs, std::integral auto rhs) -> frame_count&   { lhs.value += rhs; return lhs; }
+constexpr inline auto operator-=(frame_count& lhs, frame_count rhs) -> frame_count&          { lhs.value -= rhs.value; return lhs; }
+constexpr inline auto operator-=(std::integral auto& lhs, frame_count rhs) -> decltype(auto) { lhs -= rhs.value; return lhs; }
+constexpr inline auto operator-=(frame_count& lhs, std::integral auto rhs) -> frame_count&   { lhs.value -= rhs; return lhs; }
+constexpr inline auto operator+=(frame_idx& lhs, frame_count rhs) -> frame_idx&              { lhs.value += rhs.value; return lhs; }
+constexpr inline auto operator+=(frame_idx& lhs, std::integral auto rhs) -> frame_idx&       { lhs.value += rhs; return lhs; }
+constexpr inline auto operator/=(frame_count& lhs, std::integral auto rhs) -> frame_count&   { lhs.value /= rhs; return lhs; }
+constexpr inline auto operator/=(frame_idx& lhs, std::integral auto rhs) -> frame_idx&       { lhs.value /= rhs; return lhs; }
 constexpr inline auto operator++(frame_idx& lhs) -> frame_idx&             { lhs.value++; return lhs; }
 constexpr inline auto operator++(frame_idx& lhs, int) -> frame_idx         { auto tmp = lhs; lhs.value++; return tmp; }
 constexpr inline auto operator++(channel_count& lhs) -> channel_count&     { lhs.value++; return lhs; }
